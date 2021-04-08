ce2cff8e7bef9a424d2b6a771c179f06626846e3
F.Montalvo <F.montalvo@csu.fullerton.edu>
2021-04-07 21:19:13 -0700

    Main window set up and Capability 1 is working functionly as intended

--- Main_Window.py
+++ Main_Window.py
@@ -11,39 +11,47 @@ root.geometry('1250x700')
 root.title("Hotel Transylvania")
 root.resizable(False,False)
 
-isClicked = False
+roomlist = []
+
+class Room:
+    def __init__(self,root,counter):
+        size = ["King","Double Queen","Double Queen with Kitchen", "Suite"]
+        availability  = {"Available":"green","Unavailable/Occupied":"red", "Unavailable/Dirty":"blue", "Unavailable/Maintenance":"purple"}
+        key = ["Available","Unavailable/Occupied", "Unavailable/Dirty","Unavailable/Maintenance"]
+        self.avail = key[random.randint(0,3)]
+        self.roomsize = size[random.randint(0,3)]
+        self.color = availability.get(self.avail)
+        self.root = root
+        roomlist.append(tk.Button(self.root, text = "Room #" + str(counter+1) + ' ' + self.roomsize, command = lambda: checkRoom(self,roomlist,counter), font =("arial",12)))
+
+    def configure(self,counter):
+        roomlist[counter].configure(fg = self.color)
 
 def ShowRooms():
-    counter = 0
-    size = ["King","Double Queen","Double Queen with Kitchen", "Suite"]
-    roomlist = [0 for i in range(20)]
-    rooms = list()
+    legend = tk.Label(root, text = "Available", font = ("arial", 18))
+    legend.configure(fg = "green")
+    legend2 = tk.Label(root, text = "Unavailable/Occupied", font = ("arial", 18))
+    legend2.configure(fg = "red")
+    legend3 = tk.Label(root, text = "Unavailable/Dirty", font = ("arial", 18))
+    legend3.configure(fg = "blue")
+    legend4 = tk.Label(root, text = "Unavailable/Maintenance", font = ("arial", 18))
+    legend4.configure(fg = "purple")
+
+    legend.place(x = 300, y = 25)
+    legend2.place(x = 420, y = 25)
+    legend3.place(x = 680, y = 25)
+    legend4.place(x = 880, y = 25)
+
+    counter = 50
     i = 0
     while i < 20:
-        randsize = size[random.randint(0,3)]
-        roomlist[i] = tk.Button(root, text= "Room #" + str(i+1) + " " + randsize)
-        rooms.append([roomlist[i],isClicked])
-        roomlist
-        roomlist[i].config(command = lambda: checkRoom(rooms), font=("arial", 12))
+        Room(root,i)
         roomlist[i].place(x = 500, y = counter + 30)
         counter = counter + 30
         i = i + 1
 
-def checkRoom(rooms):
-    for i in rooms:
-        if rooms[1] == True
-    availability  = ["Available","Unavailable/Occupied", "Unavailable/Dirty", "Unavailable/Maintenance"]
-    color = ["green","red","yellow", "purple"]
-    rand = random.randint(0,3)
-    if rand == 0:
-        pick = color[0]
-    elif rand== 1:
-        pick = color[1]
-    elif  rand== 2:
-        pick = color[2]
-    elif rand== 3:
-        pick = color[3]
-    roomlist[id].configure(fg= pick)
+def checkRoom(self,rooms,counter):
+    self.configure(counter)
 
 
 
