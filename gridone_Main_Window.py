c317e8ae9b7e397db6c186f0e4b3629326bfbb85
dhruvpatel221 <72094928+dhruvpatel221@users.noreply.github.com>
2021-04-08 21:11:33 -0700

    Add files via upload

--- gridone_Main_Window.py
+++ gridone_Main_Window.py
@@ -1,20 +1,25 @@
-import tkinter as tk
+
+from tkinter import *
 import random
 
 #download the file in command prompt go to where the file is located use python example.py
 
 #This sets up the TK inter
-root = tk.Tk()
+root = Tk()
 #This sets the size of th window
 root.geometry('1250x700')
 #This sets up the Title of the Window
 root.title("Hotel Transylvania")
 root.resizable(False,False)
 
+
+frame1 = Frame(root)
+frame2 = Frame(root)
+
 roomlist = []
 
 class Room:
-    def __init__(self,root,counter,lock):
+    def __init__(self,root,counter):
         size = ["King","Double Queen","Double Queen with Kitchen", "Suite"]
         availability  = {"Available":"green","Unavailable/Occupied":"red", "Unavailable/Dirty":"blue", "Unavailable/Maintenance":"purple"}
         key = ["Available","Unavailable/Occupied", "Unavailable/Dirty","Unavailable/Maintenance"]
@@ -22,38 +27,24 @@ class Room:
         self.roomsize = size[random.randint(0,3)]
         self.color = availability.get(self.avail)
         self.root = root
-        if lock == 1:
-            roomlist.append(tk.Button(self.root, text = "Room #" + str(counter+1) + ' ' + self.roomsize, command = lambda: checkRoom(self,roomlist,counter), font =("arial",12)))
-        if lock == 2:
-            roomlist.append(tk.Label(self.root, text = "Room #" + str(counter+1) + ' ' + self.roomsize, font =("arial",12)))
-
+        roomlist.append(Button(self.root, text = "Room #" + str(counter+1) + ' ' + self.roomsize, command = lambda: checkRoom(self,roomlist,counter), font =("arial",12)))
 
     def configure(self,counter):
         roomlist[counter].configure(fg = self.color)
 
-def clear(window):
-    roomlist.clear()
-    _list = window.winfo_children()
-    for item in _list:
-        if item.winfo_children():
-            _list.extend(item.winfo_children())
-        num = _list[0:8]
-        for item in _list:
-            if item == num[0] or item == num[1] or item == num[2] or item == num[3] or item == num[4] or item == num[5]or item == num[6] or item == num[7]:
-                continue
-            item.place_forget()
-
-
-
+def clear(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+    
 def ShowRooms():
-    clear(root)
-    legend = tk.Label(root, text = "Available", font = ("arial", 18))
+    clear(frame2)
+    legend = Label(frame2, text = "Available", font = ("arial", 18))
     legend.configure(fg = "green")
-    legend2 = tk.Label(root, text = "Unavailable/Occupied", font = ("arial", 18))
+    legend2 = Label(frame2, text = "Unavailable/Occupied", font = ("arial", 18))
     legend2.configure(fg = "red")
-    legend3 = tk.Label(root, text = "Unavailable/Dirty", font = ("arial", 18))
+    legend3 = Label(frame2, text = "Unavailable/Dirty", font = ("arial", 18))
     legend3.configure(fg = "blue")
-    legend4 = tk.Label(root, text = "Unavailable/Maintenance", font = ("arial", 18))
+    legend4 = Label(frame2, text = "Unavailable/Maintenance", font = ("arial", 18))
     legend4.configure(fg = "purple")
 
     legend.place(x = 300, y = 25)
@@ -64,7 +55,7 @@ def ShowRooms():
     counter = 50
     i = 0
     while i < 20:
-        Room(root,i,1)
+        Room(root,i)
         roomlist[i].place(x = 500, y = counter + 30)
         counter = counter + 30
         i = i + 1
@@ -72,38 +63,54 @@ def ShowRooms():
 def checkRoom(self,rooms,counter):
     self.configure(counter)
 
-def roomSchedule():
-    clear(root)
-    i = 0
-    counter = 50
-    while i < 20:
-        Room(root,i,2)
-        roomlist[i].place(x = 300, y = counter + 30)
-        counter = counter + 30
-        i = i + 1
 
 
 def execute():
     print("welcome")
 
 
-Capability1 = tk.Button(root, text='Show Rooms and Status', command=ShowRooms,font=("arial", 12))
-Capability2 = tk.Button(root, text='Show Room Schedule', command=roomSchedule,font=("arial", 12))
-Capability3 = tk.Button(root, text='Customer Reservation', command=execute,font=("arial", 12))
-Capability4 = tk.Button(root, text='Housekeeping', command=execute,font=("arial", 12))
-Capability5 = tk.Button(root, text='Guest Profile', command=execute,font=("arial", 12))
-Capability6 = tk.Button(root, text='Current Stay', command=execute,font=("arial", 12))
-Capability7 = tk.Button(root, text='Search', command=execute,font=("arial", 12))
-Capability8 = tk.Button(root, text='Daily Report', command=execute,font=("arial", 12))
+def Search():
+    clear(frame2)
+    label1 = Label(frame2, text = "Guest First Name", font = ("arial", 12),height= 2).grid(row=0,column=0)
+    field1 = Entry(frame2,  font = ("arial", 12)).grid(row=0,column=1)
+    label2 = Label(frame2, text = "Guest last Name", font = ("arial", 12),height= 2).grid(row=0,column=2)
+    field2 = Entry(frame2, font = ("arial", 12)).grid(row=0,column=3)
+    label3 = Label(frame2, text = "Room Number", font = ("arial", 12),height= 2).grid(row=0,column=4)
+    field3 = Entry(frame2, font = ("arial", 12)).grid(row=0,column=5)
+    label4 = Label(frame2, text = "Phone Number", font = ("arial", 12),height= 2).grid(row=0,column=6)
+    field4 = Entry(frame2, font = ("arial", 12)).grid(row=0,column=7)
+    label5 = Label(frame2, text = "Street Address", font = ("arial", 12) ,height= 2).grid(row=1,column=0)
+    field5 = Entry(frame2, font = ("arial", 12)).grid(row=1,column=1)
+    label5 = Label(frame2, text = "Check In Date", font = ("arial", 12) ,height= 2).grid(row=1,column=2)
+    field5 = Entry(frame2, font = ("arial", 12)).grid(row=1,column=3)
+    label6 = Label(frame2, text = "Check In Date", font = ("arial", 12) ,height= 2).grid(row=1,column=4)
+    field6 = Entry(frame2, font = ("arial", 12)).grid(row=1,column=5)
+    Button1 = Button(frame2, text='Search',font=("arial", 14)).grid(row=1,column=6)
+    frame2.grid(row=1,column=0)
+    
 
+def report():
+    clear(frame2)
 
-Capability1.place(x = 0,y = 0)
-Capability2.place(x = 0,y = 30)
-Capability3.place(x = 0,y = 60)
-Capability4.place(x = 0,y = 90)
-Capability5.place(x = 0,y = 120)
-Capability6.place(x = 0,y = 150)
-Capability7.place(x = 0,y = 180)
-Capability8.place(x = 0,y = 210)
+    label1 = Label(frame2, text = "Today's report", font = ("arial", 20),height= 2).grid(row=0,column=0)
+    frame2.grid(row=1,column=0)
+
+Capability1 = Button(frame1, text='Show Rooms and Status', command=ShowRooms,font=("arial", 12), width=20,height=5)
+Capability1.grid(row = 0, column=0)
+Capability2 = Button(frame1, text='Show Room Availability', command=execute,font=("arial", 12), width=20,height=5)
+Capability2.grid(row = 0, column=1)
+Capability3 = Button(frame1, text='Customer Reservation', command=execute,font=("arial", 12), width=20,height=5)
+Capability3.grid(row = 0, column=2)
+Capability4 = Button(frame1, text='Housekeeping', command=execute,font=("arial", 12), width=20,height=5)
+Capability4.grid(row = 0, column=3)
+Capability5 = Button(frame1, text='Guest Profile', command=execute,font=("arial", 12), width=20,height=5)
+Capability5.grid(row = 0, column=4)
+Capability6 = Button(frame1, text='Current Stay', command=execute,font=("arial", 12), width=20,height=5)
+Capability6.grid(row = 0, column=5)
+Capability7 = Button(frame1, text='Search', command=Search,font=("arial", 12), width=20,height=5)
+Capability7.grid(row = 0, column=6)
+Capability8 = Button(frame1, text='Daily Report', command=report,font=("arial", 12), width=20,height=5)
+Capability8.grid(row = 0, column=7)
+frame1.grid(row=0,column=0)
 
 root.mainloop()
